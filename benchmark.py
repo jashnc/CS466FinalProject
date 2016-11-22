afef3ace2bb93e64a0ac769cad48225329570978
Matthew <mmroman2@illinois.edu>
2016-11-22 13:50:39 -0600

    Added step 1 and 2 programs and descriptions as well as fake_data to practice step 2 with

---
+++ benchmark.py
@@ -0,0 +1,13 @@
+# Implement Step 1, generate the datasets
+
+# Input:
+	# (a) A positive number called ICPC (“information content per column”)
+	# (b) A positive integer called ML (“motif length”)
+	# (c) A positive integer called SL (“sequence length”)
+	# (d) A positive integer called SC (“sequence count”)
+
+# Output
+	# (a) A file containing the sequences called "sequences.fa"
+	# (b) A file containing the locations of planted sites called "sites.txt"
+	# (c) A file containing the motif in a specified format called "motif.txt"
+	# (d) A file containing the motif length called "motiflength.txt"
---
+++ motifFinder.py
@@ -0,0 +1,9 @@
+# Implement Step 2, the algorithm
+
+# Input:
+	# (a) A file containing the sequences called "sequences.fa"
+	# (b) A file containing the motif length called "motiflength.txt"
+
+# Output
+	# (a) A file containing the predicted locations of sites called "predictedsites.txt"
+	# (b) A file containing the predicted motif in a specified format called "predictedmotif.txt"
