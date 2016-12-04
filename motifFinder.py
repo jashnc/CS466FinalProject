5c0a3aac24979a7e765addea3b39fc175c0f0e37
romanomatthew23 <romanomatthew23@gmail.com>
2016-12-03 22:31:37 -0600

    Finished motifFinder.py and verified via testing with the fake_data I made

--- motifFinder.py
+++ motifFinder.py
@@ -4,11 +4,12 @@
 	# (a) A file containing the sequences called "sequences.fa"
 	# (b) A file containing the motif length called "motiflength.txt"
 
-# Output
+# Output:
 	# (a) A file containing the predicted locations of sites called "predictedsites.txt"
 	# (b) A file containing the predicted motif in a specified format called "predictedmotif.txt"
 
 import math
+import copy
 
 ####################################################################################################
 #	        										   #
@@ -39,9 +40,7 @@ def informationContent(theSequences):
         # Loop over every sequence provided to populate the profileMatrix
         # increment the proper entries as you loop over them
         for oneSeq in theSequences:
-                #print(oneSeq)
                 for i in range(len(oneSeq)):
-                        #print(i)
                         if(oneSeq[i]=='A'):
                                 profileMatrix[i][0] = profileMatrix[i][0] + 1
                         elif(oneSeq[i]=='C'):
@@ -51,7 +50,6 @@ def informationContent(theSequences):
                         elif(oneSeq[i]=='T'):
                                 profileMatrix[i][3] = profileMatrix[i][3] + 1
 
-        #print(profileMatrix)
         #now actually calculate the information content of the profile matrix
         infoContent = 0.0
         for col in profileMatrix:
@@ -68,7 +66,7 @@ def informationContent(theSequences):
 
 ####################################################################################################
 #									        		   #
-# 			                0) Read in the input files	                           #
+# 			                1) Read in the input files	                           #
 #												   #
 ####################################################################################################
 
@@ -85,28 +83,20 @@ sequence = []   #list of sequences
 for line in Seq:
         line.rstrip('\r\n')
         if(line[0] == ">"):
-                #print("NEW SEQUENCE")
-                #print(" ")
                 nextSeq = []
                 sequence.append(nextSeq)
                 idx = idx + 1
         else:
                 sequence[idx].append(line.rstrip('\r\n'))
-                #print (" ")
-                #print (line.rstrip('\r\n'))
-                #print (" ")
 
-print(sequence)
 Seq.close()
 
-
-
-########################################################################################################
-#																									   #
-# 		                 1) Align sequences 1 and 2 together by trying 								   
-#				           every combination and maximizing the score	   							   
-#																									   #
-########################################################################################################
+####################################################################################################
+#									        		   #
+# 			         2) Align sequences 1 and 2 together              	           #
+#												   #
+####################################################################################################
+# Try every combination and maximize the score to obtain this alignment
 
 # Loop over every possible starting positions of seq 1 and 2
 # Keep Track of the best alignment and the score for that alignment
@@ -114,26 +104,101 @@ bestI = 0
 bestJ = 0
 bestScore = 0
 for i in range(len(sequence[0][0]) - motifLength):
-        for j in range(len(sequence[0][0]) - motifLength):
-                infoContentInput = []
-                infoContentInput.append(sequence[0][0][i:(i+motifLength)])
-                infoContentInput.append(sequence[1][0][j:(j+motifLength)])
-                if(informationContent(infoContentInput) > bestScore):
-                        bestScore = informationContent(infoContentInput)
-                        bestI = i
-                        bestJ = j
+	for j in range(len(sequence[0][0]) - motifLength):
+		infoContentInput = []
+		infoContentInput.append(sequence[0][0][i:(i+motifLength)])
+		infoContentInput.append(sequence[1][0][j:(j+motifLength)])
+		score = informationContent(infoContentInput) 
+		if(score > bestScore):
+			bestScore = score
+			bestI = i
+			bestJ = j
 
-print("Best Alignment of first 2 sequences = ")
-print(sequence[0][0][bestI:(bestI+motifLength)])
-print(sequence[1][0][bestJ:(bestJ+motifLength)])
+#keep a variable for the best alignment, add on to it later incrementally
+alignment = []
+alignment.append(sequence[0][0][bestI:(bestI+motifLength)])
+alignment.append(sequence[1][0][bestJ:(bestJ+motifLength)])
+
+#keep variable for predictedSites
+predictedSites = []
+predictedSites.append(bestI)
+predictedSites.append(bestJ)
+
+####################################################################################################
+#									        		   #
+# 			       3) Iteratively add the remaining sequences                          #
+#												   #
+####################################################################################################
+# Maintain the current alignment, and try each position of the next sequence to maximize the score 
+# 	and align the sequence
+
+#iterate over all remaining sequences
+for i in range(2,len(sequence)):
+	#initialize maximizing variables
+	bestJ = 0
+	bestScore = 0
+	#for this sequence iterate over every possible starting position
+	for j in range(len(sequence[0][0]) - motifLength):
+		newAlignment = copy.deepcopy(alignment)
+		newAlignment.append(sequence[i][0][j:(j+motifLength)])
+		score = informationContent(newAlignment)
+		if(score > bestScore):
+			bestScore = score
+			bestJ = j
+	#after done iterating, update current alignment and sites
+	alignment.append(sequence[i][0][bestJ:(bestJ+motifLength)])
+	predictedSites.append(bestJ)
+
+####################################################################################################
+#									        		   #
+# 		         3) Output the motif we found			                           #
+#												   #
+####################################################################################################
+
+# Convert the alignment to a profilematrix
+#make a 2D list (matrix) to represent the profile matrix profileMatrix[col][row]
+#initialize each entry to 0
+profileMatrix = []
+for i in range(len(alignment[0])):
+        newDim = []
+        for j in range(4):
+                newDim.append(0)
+        profileMatrix.append(newDim)
+
+# Loop over every sequence provided to populate the profileMatrix
+# increment the proper entries as you loop over them
+for oneSeq in alignment:
+        for i in range(len(oneSeq)):
+                if(oneSeq[i]=='A'):
+                        profileMatrix[i][0] = profileMatrix[i][0] + 1
+                elif(oneSeq[i]=='C'):
+                        profileMatrix[i][1] = profileMatrix[i][1] + 1
+                elif(oneSeq[i]=='G'):
+                        profileMatrix[i][2] = profileMatrix[i][2] + 1
+                elif(oneSeq[i]=='T'):
+                        profileMatrix[i][3] = profileMatrix[i][3] + 1
+
+# Finally, output this profile Matrix
+f = open("predictedmotif.txt", "w")
+f.write(">MOTIF1 " + str(motifLength) + "\n")
+for col in profileMatrix:
+	f.write(str(col[0]) +" " + str(col[1]) + " " + str(col[2]) + " " + str(col[3]) + "\n")
+f.write("<\n")
+f.close()
+
+		
+####################################################################################################
+#									        		   #
+# 		         4) Output the predictedSites we found		                           #
+#												   #
+####################################################################################################
+f = open("predictedsites.txt", "w")
+for site in predictedSites:
+	f.write(str(site+1) + "\n")
+
+f.close()
 
 
-########################################################################################################
-#																									   #
-# 		              1) Iteratively add another sequence to the current alignment by 				   
-#				     trying each position in the new sequence and maximizing the score   			   
-#																									   #
-########################################################################################################
 
 
 
